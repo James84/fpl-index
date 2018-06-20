462a71405312e596b2a3ba1897b87e4c5abb16c8
James Runham <james.runham@reedonline.co.uk>
2018-06-20 15:57:37 +0100

    added player request

---
+++ FantasyLeague/ReactApp/actions/players.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+export const SEARCH_PLAYERS = 'SEARCH_PLAYERS';
+export const GET_PLAYER = 'GET_PLAYER';
+
+const ROOT = 'http://fantasy.league.local';
+
+export function searchPlayers(lastName) {
+    const url = `${ROOT}/search/players/prefix?lastname=${lastName}`;
+//    console.log('search player action called!');
+
+    const request = axios.get(url);
+        
+    return {
+        type: SEARCH_PLAYERS,
+        payload: request
+    };
+}
+
+export function getPlayer(id){
+    const url = `${ROOT}/search/players/id/${id}`;
+    
+    const request = axios.get(url);
+    
+    return {
+        type: GET_PLAYER,
+        payload: request
+    }
+}
