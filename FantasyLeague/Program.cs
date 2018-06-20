b9b18c28fb81a3a1127fceba4af09ff46313ae3c
James Runham <james.runham@reedonline.co.uk>
2018-06-20 11:11:07 +0100

    set up iis

--- FantasyLeague/Program.cs
+++ FantasyLeague/Program.cs
@@ -19,6 +19,7 @@ namespace FantasyLeague
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseIISIntegration()
                 .UseStartup<Startup>();
     }
 }
